576e81bb7e9c5b2ef4a06046d4d14911604e9a71
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-28 11:34:12 -0700

    psuedo code for App Test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,3 +4,17 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 // test list displays
+
+// character data
+// setupServer (rest.get, ctx)
+
+describe('App', () => {
+  //beforeAll
+  //afterAll
+
+  it('shoulder render a list of characters', async () => {
+    // render App
+    // loading
+    // find character
+  });
+});
