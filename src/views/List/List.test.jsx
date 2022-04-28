2847953a835a226ed65d1d31d68d33bd9c7fcb31
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-28 11:30:38 -0700

    psuedo code for List Test

--- src/views/List/List.test.jsx
+++ src/views/List/List.test.jsx
@@ -1,1 +1,20 @@
 // test list filtering by nation
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import List from '../List/List';
+
+// character data
+// setupServer (rest.get, ctx)
+
+describe('Character List', () => {
+  // beforeAll, afterAll
+
+  it('should render a list of characters that are filterable by dropdown', async () => {
+    // render
+    // loading message
+    // filter with dropdown
+    // expect name/character
+  })
+})
