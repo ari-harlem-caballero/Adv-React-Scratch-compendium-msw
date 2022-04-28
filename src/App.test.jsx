c2cb08b4289100e46f5b4110f56e3be7afeb385c
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-28 13:45:19 -0700

    setupTest refactor

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,44 +1,8 @@
 // test list displays
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-// character data
-const data = [
-  {
-    _id: '5cf5679a915ecad153ab6a45',
-    name: 'Sokka',
-    photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/c/cc/Sokka.png/revision/latest?cb=20140905085428',
-    affiliation: 'Water Tribe'
-  },
-  {
-    _id: '5cf5679a915ecad153ab6ab6',
-    name: 'Zuko',
-    photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/4/4b/Zuko.png/revision/latest?cb=20180630112142',
-    affiliation: 'Fire Nation'
-  },
-  {
-    _id: '5cf5679a915ecad153ab69dd',
-    name: 'Momo',
-    photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/4/43/Inquisitive_Momo.png/revision/latest?cb=20081225191217',
-    affiliation: 'Air Nomads'
-  }
-];
-
-// setupServer (rest.get, ctx)
-const server = setupServer(
-  rest.get('https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=500', (req, res, ctx) =>
-  res(ctx.json([data]))
-  )
-);
-
 describe('App', () => {
-  //beforeAll
-  beforeAll(() => server.listen());
-  //afterAll
-  afterAll(() => server.close());
-
   it('shoulder render a list of characters', async () => {
     // render App
     render(<App />)
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,39 @@
-global.fetch = (...args) =>
-  import('cross-fetch').then(({ default: fetch }) => fetch(...args));
+import fetch from 'cross-fetch';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+global.fetch = fetch;
+
+// character data
+const data = [
+  {
+    _id: '5cf5679a915ecad153ab6a45',
+    name: 'Sokka',
+    photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/c/cc/Sokka.png/revision/latest?cb=20140905085428',
+    affiliation: 'Water Tribe'
+  },
+  {
+    _id: '5cf5679a915ecad153ab6ab6',
+    name: 'Zuko',
+    photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/4/4b/Zuko.png/revision/latest?cb=20180630112142',
+    affiliation: 'Fire Nation'
+  },
+  {
+    _id: '5cf5679a915ecad153ab69dd',
+    name: 'Momo',
+    photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/4/43/Inquisitive_Momo.png/revision/latest?cb=20081225191217',
+    affiliation: 'Air Nomads'
+  }
+];
+
+// setupServer (rest.get, ctx)
+const server = setupServer(
+  rest.get('https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=500', (req, res, ctx) =>
+  res(ctx.json([data]))
+  )
+);
+
+//beforeAll
+beforeAll(() => server.listen());
+//afterAll
+afterAll(() => server.close());
