8f531ad486d7c2d04bb9eee89db6ff717883404f
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-28 18:08:10 -0700

    List: set state

--- src/views/List/List.jsx
+++ src/views/List/List.jsx
@@ -1,10 +1,15 @@
 // holds list of character cards, dropdown component, filters
 import { useEffect, useState } from 'react';
 // import: CharacterCard, NationDropdown
-
+import CharacterCard from '../../components/Character/CharacterCard';
+import NationDropdown from '../../components/Dropdowns/NationDropdown';
 
 export default function CharacterList() {
   // set state: loading, results, filter
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState([]);
+
   // useEffect: fetchCharacters (data.map: id, photoUrl, name, affiliation)
   // handleSearch: filter characters based on nation (dropdown holder)
   // return:
